4f5a16b23b55ef9659223eca9e6ac73bff2e70aa
leonzv <leonzv19@gmail.com>
2023-04-23 04:17:18 -0300

    promise para pegar ultima versão do jogo automaticamente

--- scp.js
+++ scp.js
@@ -1,11 +1,37 @@
 window.onload = function(){
   var refreshBtn = document.getElementById("refreshBtn");
-  
+  var version;
   refreshBtn.addEventListener("click", getChampionData); 
+
+  //promise para pegar a ultima versão do jogo e aplicar em uma variavel para ser colocada nos links para não precisar mudar manualmente
+  //explicação do que é promise:
+  //promise é uma função que retorna um objeto que representa o sucesso ou falha de uma operação assíncrona
+  //tive que fazer promise porque a variavel tinha que estar pronta antes de ser usada
+  function getVersion() {
+    return new Promise(function(resolve, reject) {
+      axios.get('https://ddragon.leagueoflegends.com/api/versions.json').then(function (response) {
+        version = response.data[0];
+        resolve(version);
+      })
+      .catch(function (error) {
+        console.log(error);
+        reject(error);
+      });
+    });
+  }
+
+  getVersion().then(function(version) {
+    //esse código só é executado depois que a Promise é resolvida
+    console.log('Versão:', version);
+    getChampionData();
+  })
+  .catch(function(error) {
+    console.log(error);
+  });  
 
   function getChampionData() {
   //faz uma solicitação GET para a API Dragon para pegar os dados de um campeão aleatório
-  axios.get('https://ddragon.leagueoflegends.com/cdn/13.8.1/data/pt_BR/champion.json')
+  axios.get(`https://ddragon.leagueoflegends.com/cdn/${version}/data/pt_BR/champion.json`)
   .then(function (response) {
       // pega o objeto com info sobre os campeões
     const champions = response.data.data;
@@ -28,25 +54,16 @@ window.onload = function(){
     const championType = randomChampion.tags[0];
 
     // faz uma solicitação GET para a API pra ter informações sobre build de acordo com o tipo de poder do campeão
-    axios.get(`https://ddragon.leagueoflegends.com/cdn/13.8.1/data/pt_BR/item.json?tags=${championType}`)
+    axios.get(`https://ddragon.leagueoflegends.com/cdn/${version}/data/pt_BR/item.json?tags=${championType}`)
     .then(function (response) { 
         // pega o objeto com info sobre os itens
-        const items = response.data.data;
+        const items = response.data.data; 
         
-        // pega uma lista com os nomes dos itens
-        const itemNames = Object.keys(items);
-
-        // seleciona aleatoriamente seis ids
-        const randomItemIds = [];
-        for (let i = 0; i < 6; i++) {
-            randomItemIds.push(itemNames[Math.floor(Math.random() * itemNames.length)]);
-        }
-
         //cria um objeto com as info dos campeões e itens selecionados
         const championData = {
           name: randomChampionName,
           splashArt: `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${randomChampionImage}_0.jpg`,
-          build: randomItemIds,
+          build: "wip",
           powerType: championType
         };
 
